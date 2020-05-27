535d819b95397f8287a6fb058c7683e338163a48
Jesús González Álvarez <jesusgranada99@gmail.com>
2020-05-27 17:55:26 +0200

    Creo la interfaz y documentación de la clase Imagen

---
+++ include/Imagen.h
@@ -0,0 +1,191 @@
+/**
+  * @file Imagen.h
+  * @brief Interfaz de la clase `Imagen`
+  *
+  */
+
+#ifndef _IMAGEN_H_
+#define _IMAGEN_H_
+
+#include <string.h>
+
+using namespace std;
+
+/**
+  * @class Imagen
+  *
+  */
+
+typedef unsigned char pixel;
+
+class Imagen{
+	private:
+		/**
+		  * @brief Filas
+ 		  * @pre 0 <= fils
+		  */
+		int fils;
+
+		/**
+		  * @brief Columnas
+ 		  * @pre 0 <= cols
+		  */
+		int cols;
+
+		/**
+		  * @brief Máximo valor de luminosidad
+		  * @pre 0 <= max_luminosidad
+		  */
+		int max_luminosidad;
+
+		/**
+		  * @brief Contenido de la imagen
+		  */
+		pixel ** img;
+
+
+		/**
+		  * @brief Borra la imagen
+		  */
+		void LiberaEspacio ();
+
+		/**
+		  * @brief Reserva espacio para guardar una imagen con las dimensiones
+		   	   	   pasadas como argumento
+		  * @param f : filas a reservar
+		  * @param c : columnas a reservar
+		  * @pre `f` >= 0
+		  * @pre `c` >= 0
+		  * @post `fils` = `f`
+		  * @post `cols` = `c`
+		  * @post `img` tiene espacio para `fxc` píxeles
+		  */
+		void ReservaEspacio (const int f, const int c);
+
+	public:
+
+		/**
+		  * @brief Constructor por defecto
+		  * @brief Se inicializa como una imagen vacía (0x0)
+		  */
+		Imagen ();
+
+		/**
+		  * @brief Constructor por copia
+		  * @param otra Imagen a copiar
+		  */
+		Imagen (const Imagen & otra);
+
+		/**
+		  * @brief Constructor. Crea una imagen rectangular vacía.
+		  * @param f filas
+		  * @param c columnas
+		  * @param v valor al que se inicializan los píxeles de la imagen
+		  * @post Se crea una imagen de `fxc` dimensiones cuyos píxeles valen v
+		  */
+		Imagen (const int f = 0, const int c = 0, const int valor = 0);
+
+		/**
+		  * @brief Constructor. Crea una imagen a partir de un fichero PGM.
+		  * @param nombre_fichero nombre del fichero PGM
+		  */
+		Imagen (string nombre_fichero);
+
+		/**
+		  * @brief Destructor
+		  */
+		~Imagen ();
+
+		/**
+		  * @brief Consultor para filas
+		  * @return `fils`
+		  */
+		int GetFils() const;
+
+		/**
+		  * @brief Consultor para columnas
+		  * @return `cols`
+		  */
+		int GetCols() const;
+
+		/**
+		  * @brief Consultor/Modificador del píxel en la posición
+		  		   `fila x columna`
+		  * @param fila Fila en la que se encuentra el píxel
+		  * @param columna Columna en la que se encuentra el píxel
+		  * @pre (fila>=0) && (fila<=fils)
+		  * @pre (columna>=0) && (columna<=cols)
+		  * @return Devuelve una referencia al píxel apuntado por
+		  	`fila x columna`
+		  */
+		int & Pixel (const int fila, const int columna);
+
+		/**
+		  * @brief Devuelve el número de comentarios guardados
+		  * @return Número de comentarios guardados
+		  */
+		int GetNumComentarios(void) const;
+
+		/**
+		  * @brief Consulta un comentario dado por un índice
+		  * @param index índice del comentario a consultar
+		  * @return Devuelve el comentario en el índice `index` o un string
+		            vacío si `index < 0  ||  index >= GetNumComentarios()`
+	 	  */
+		string GetComentario (const int index) const;
+
+		/**
+		  * @brief Guarda la imagen como PGM P2 en el fichero de salida
+		  * @param out nombre del fichero donde se guardará la imagen
+		  */
+		void ToP2 (const string &out);
+
+		/**
+		  * @brief Guarda la imagen como PGM P5 en el fichero de salida
+		  * @param out nombre del fichero donde se guardará la imagen
+		  * @post Se añade el comentario "Reiniciada desde 'out'"
+		  */
+		void ToP5 (const string &out);
+
+		/**
+		  * @brief Operador de asignación
+		  * @param otro : Imagen a copiar
+		  * @post Se añade el comentario: "Reiniciada
+		          desde otro objeto de la clase"
+		  * @return Devuelve una referencia al objeto apuntado por `this`
+	 	  */
+		Imagen & operator = (const Imagen &otra);
+
+		/**
+		  * @brief Operador de asignación
+		  * @param valor : Valor asignado a todos los píxeles
+		  * @post Se añade el comentario: "Reiniciada al valor `valor`"
+		  * @return Devuelve una referencia al objeto apuntado por `this`
+	 	  */
+		Imagen & operator = (const int valor);
+
+		/**
+		  * @brief Operador `!`
+		  * @brief Calcula el negativo de la imagen
+		  * @post Se altera el valor de cada píxel a su negativo
+		  * @post Añade el comentario: `Negativo`
+		  * @return Devuelve una referencia al objeto apuntado por `this`
+	 	  */
+		Imagen & operator ! ();
+
+		/**
+		  * @brief Operador `!`
+		  * @brief Crea una imagen en la que sólo es visible lo índicado por la
+		  *        máscara
+		  * @param mascara Imagen que actúa como máscara
+		  * @post Añade una línea formada por guiones a los comentarios y
+		          los comentarios de `mascara`.
+		  * @post Añade una línea formada por guiones a los comentaros y
+		          el comentario: `Creada por: ENMASCARAMIENTO`
+		  * @post Si no tienen el mismo tamaño se devuelve una imagen vacía
+		  * @return Devuelve una referencia al objeto apuntado por `this`
+	 	  */
+		Imagen & operator * (const Imagen & mascara);
+};
+
+#endif
