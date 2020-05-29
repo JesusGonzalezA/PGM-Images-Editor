ff80d4442483628dbe46a676115070006020eec5
Jesús González Álvarez <jesusgranada99@gmail.com>
2020-05-29 16:37:07 +0200

    Aplicación Negativizar imagen terminada

--- src/Negativo.cpp
+++ src/Negativo.cpp
@@ -15,7 +15,49 @@
 
 using namespace std;
 //*****************************************************************************
-int main (){
+int main (int argc, char **argv){
+
+	//Compruebo los argumentos
+	if (argc > 3 || argc==1)
+	{
+		cerr << "Error: los argumentos no son válidos." << endl;
+		cerr << "Uso correcto:" << endl
+			 << argv[0] << " <fichero_Imagen>  : se altera la imagen." << endl
+			 << argv[0] << " <fichero_Imagen>  <fichero_salida>" << endl;
+
+		exit (1);
+	}
+
+	//Preguntar al usuario si quiere sobreescribir los datos
+	string nombre_fichero = (argc == 2)? argv[1] : argv[2];
+	char respuesta;
+
+	if (ExisteFichero(nombre_fichero)){
+		do {
+			cout << endl;
+			cout << "¿Desea sobreescribir los datos en el fichero "
+				 << nombre_fichero << "? (S | N) : ";
+
+			cin >> respuesta;
+
+		}while (respuesta != 'S' && respuesta != 'N');
+	}
+
+	//MENSAJE PARA TERMINAR
+	if (respuesta == 'N')
+	{
+		cout << endl;
+		cout << "Se termina la ejecución del programa sin sobreescribir "
+		     << "la imagen" << endl;
+	}
+
+	//PROCESAMIENTO
+	else{
+		Imagen entrada(argv[1]);
+		Imagen copia(entrada);
+		(!copia).ToP2(nombre_fichero);
+	}
+
 	return 0;
 }
 //*****************************************************************************
